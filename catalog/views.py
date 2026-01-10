09007f591a0b39a8d6ebdf24c4201f77fc4eba8f
Anton Drevin <a.drevin@mail.ru>
2026-01-10 17:10:37 +0400

    настройка HTML

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from catalog.models import Product
 from catalog.utils import greeting
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'home.html', context=context)
 
 def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         return HttpResponse(f"{greeting()}, {name}! Мы получили Ваше сообщение")
     return render(request, 'contacts.html')
+
+def product_card(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {'product': product}
+    return render(request, 'product_card.html', context=context)
