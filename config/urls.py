4ff5caef4de93552ac7a542eb1e776144465939c
Anton Drevin <a.drevin@mail.ru>
2026-01-19 16:38:03 +0400

    создание blog

--- config/urls.py
+++ config/urls.py
@@ -12,7 +12,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blogasd/', include('blogasd.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -23,7 +23,8 @@ from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls', namespace='catalog'))
+    path('', include('catalog.urls', namespace='catalog')),
+    path('', include('blog.urls', namespace='blog'))
 ]
 
 if settings.DEBUG:
