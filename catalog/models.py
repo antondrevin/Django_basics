a4d33fa50b8e6c107bf9982fca3d641324076edf
Anton Drevin <a.drevin@mail.ru>
2026-01-21 19:14:54 +0400

    Финальная настройка проекта 26.1

--- catalog/models.py
+++ catalog/models.py
@@ -39,6 +39,7 @@ class Product(models.Model):
     price = models.DecimalField(
         max_digits=10, decimal_places=2, verbose_name="Цена за покупку", help_text="Введите цену товара"
     )
+    is_active = models.BooleanField(default=True, verbose_name='Доступен')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
 
